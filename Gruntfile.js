42290d4787ad279ab0f57ee707c4570c33c731bd
Alex Beauchemin <abeauchemin@u92.com>
2015-07-07 16:14:24 -0400

    Added sass files Added sass and postcss to the grunt task Added css minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,20 +10,62 @@ module.exports = function(grunt){
       less_main: {
         files: './css/**/*.less',
         tasks: ['less']
+      },
+      sass: {
+        files: './css/**/*.scss',
+        tasks: ['sass', 'postcss:autoprefixer', 'postcss:minify']
       }
     },
 
     less: {
       main: {
         files: {
-          './css/style.css': './css/style.less'
+          './css/style.css': './css/less/style.less'
+        }
+      }
+    },
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          './css/style.css': './css/sass/style.scss'
+        }
+      }
+    },
+
+    postcss: {
+      autoprefixer: {
+        options: {
+          map: true,
+          processors: [
+            require('autoprefixer-core')
+          ]
+        },
+        files: {
+          './css/style.css': './css/style.css'
+        }
+      },
+      minify: {
+        options: {
+          map: true,
+          processors: [
+            require('csswring')
+          ]
+        },
+        files: {
+          './css/style.min.css': './css/style.css'
         }
       }
     }
-
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-postcss');
 
+  grunt.registerTask('default', ['sass', 'postcss:autoprefixer', 'postcss:minify', 'watch']);
 };
